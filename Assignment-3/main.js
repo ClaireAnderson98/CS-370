39b6008f809c8dca949a436afe5bf52cadccbfc1
ClaireAnderson98 <47344823+ClaireAnderson98@users.noreply.github.com>
2023-03-01 11:20:30 -0800

    Update main.js

--- Assignment-3/main.js
+++ Assignment-3/main.js
@@ -1,1 +1,18 @@
 //main.js
+function init() {
+  var canvas = document.getElementById("webgl-canvas");
+  gl = canvas.getContext("webgl2");
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.enable(gl.DEPTH_TEST);
+  cube = new Cube (gl);
+  render();
+  requestAnimationFrame(render);
+}
+
+function render() {
+  gl.clear(gl.COLOR_BUFFER_BIT |
+           gl.DEPTH_BUFFER_BIT);
+  cube.render();
+  requestAnimationFrame(render);
+}
+window.onload = init;
