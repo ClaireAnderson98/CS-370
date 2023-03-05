17e8b1cdd8850d7ccb2ffdd006c3104fe5cfc6b9
ClaireAnderson98 <47344823+ClaireAnderson98@users.noreply.github.com>
2023-03-05 15:34:51 -0800

    Update main.js

--- Assignment-2/main.js
+++ Assignment-2/main.js
@@ -1,8 +1,8 @@
 //main.js
-functino init() {
+function init() {
 var canvas = document.getElementById("webgl-canvas");
   gl = canvas.getContext("webgl2");
-  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
   cone = new Cone(gl, 20);
   render();
 }
