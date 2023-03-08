39b1d91a570e142655a71f3d24721ab19cddbd87
ClaireAnderson98 <47344823+ClaireAnderson98@users.noreply.github.com>
2023-03-07 16:52:31 -0800

    Update Cube.js

--- Assignment-3/Cube.js
+++ Assignment-3/Cube.js
@@ -1,9 +1,12 @@
 //Cube.js
 function Cube (gl, vertexShaderId, fragmentShaderId) {
-  const vertShdr = vertexShaderId || "Cube-vertex-shader";
-  const fragShdr = fragmentShaderId || "Cube-fragment-shader";
-  let shaderProgram = initShaders (gl, vertShdr, fragShdr);
-  if (shaderProgram < 0) {
+  
+  var vertShdr = vertexShaderId || "Cube-vertex-shader";
+  var fragShdr = fragmentShaderId || "Cube-fragment-shader";
+  
+  this.program = initShaders (gl, vertShdr, fragShdr);
+  
+  if (this.program < 0) {
     alert ("Error: Cube shader pipeline failed to compile\n\n" +
            "\tvertex shader id: \t" + vertShdr + "\n" +
            "\tfragment shader id: \t" + fragShdr + "\n");
@@ -25,12 +28,12 @@ function Cube (gl, vertexShaderId, fragmentShaderId) {
                0.5, -0.5, -0.5]; //vertex 7
   indices = [ 0, 1, 3, 2, 4, 5, 6, 7, 0, 1, 2, 7, 5, 6, 4, 0, 3];
   const count = indices.length;
-  let aPosition = new Attribute (gl, shaderProgram, positions, "aPosition", 3, gl.FLOAT);
+  let aPosition = new Attribute (gl, this.program, positions, "aPosition", 3, gl.FLOAT);
   let indices = new Indices (gl, indices);
   this.render = function () {
     gl.uniformMatrix4fv(uniforms.MV, false, flatten(this.MV));
     gl.uniformMatrix4fv(uniforms.P, false, flatten(this.P));
-    gl.useProgram (shaderProgram);
+    gl.useProgram (this.program);
     aPosition.enable();
     indices.enable();
     gl.drawElements (gl.TRIANGLE_STRIP, count, indices.type, 0);
