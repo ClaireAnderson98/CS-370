4d42306eb4e06826fe0cbaacd70c0b436235acae
Claire Anderson <claire@Claires-Laptop.local>
2023-03-08 16:52:56 -0800

    fixed indicies

--- Assignment-3/Cube.js
+++ Assignment-3/Cube.js
@@ -18,7 +18,7 @@ function Cube (gl, vertexShaderId, fragmentShaderId) {
   };
   this.P = mat4();
   this.MV = mat4();
-  positions = [ 0.5, 0.5, 0.5,   //vertex 0
+  var positions = [ 0.5, 0.5, 0.5,   //vertex 0
                0.5, 0.5, -0.5,   //vertex 1
                -0.5, 0.5, -0.5,  //vertex 2
                -0.5, 0.5, 0.5,   //vertex 3
@@ -26,10 +26,10 @@ function Cube (gl, vertexShaderId, fragmentShaderId) {
                -0.5, -0.5, -0.5, //vertex 5
                0.5, -0.5, 0.5,   //vertex 6
                0.5, -0.5, -0.5]; //vertex 7
-  indices = [ 0, 1, 3, 2, 4, 5, 6, 7, 0, 1, 2, 7, 5, 6, 4, 0, 3];
+  var indices = [ 0, 1, 3, 2, 4, 5, 6, 7, 0, 1, 2, 7, 5, 6, 4, 0, 3];
   const count = indices.length;
   let aPosition = new Attribute (gl, this.program, positions, "aPosition", 3, gl.FLOAT);
-  let indices = new Indices (gl, indices);
+  indices = new Indices (gl, indices);
   this.render = function () {
     gl.uniformMatrix4fv(uniforms.MV, false, flatten(this.MV));
     gl.uniformMatrix4fv(uniforms.P, false, flatten(this.P));
