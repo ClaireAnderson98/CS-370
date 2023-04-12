da3c1e1052baba930e6e03d0ac2a94e661b78a12
Claire Anderson <claire@Claires-Laptop.local>
2023-04-12 16:42:00 -0700

    added animation

--- Assignment-3/main.js
+++ Assignment-3/main.js
@@ -9,13 +9,24 @@ function init() {
   requestAnimationFrame(render);
 }
 
+var pi = 3.14159;
+var time = 1;
 function render() {
   gl.clear(gl.COLOR_BUFFER_BIT |
            gl.DEPTH_BUFFER_BIT);
-  cube.MV = rotateX(1);
+  time = time + 1;
   cube.P = perspective (90, 1.0, 1, 3);
-  cube.MV = translate(0, 0, -2);
+  
+  ms = new MatrixStack();
+  
+  var V = translate (0, 0, -2);
+  ms.load (V);
+  
+  ms.push();
+  ms.rotate (time, [1,3,2]);
+  cube.MV = ms.current();
   cube.render();
+  ms.pop();
   requestAnimationFrame(render);
 }
 window.onload = init;
